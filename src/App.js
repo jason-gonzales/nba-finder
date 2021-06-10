3d1355163ba301c38c9cab03c0ad400fd51bdfb5
Jason Gonzales <jasongonzo25@gmail.com>
2021-06-09 21:23:02 -0700

    work in progress

--- src/App.js
+++ src/App.js
@@ -10,39 +10,33 @@ import ThemeContext, {themes} from './theme-context';
 export default function App() {
 
   const [view, setView] = useState('')
-  const [theme, setTheme] = useState(themes.home)
+
 
   const changeView = (name) => {
     setView({ name })
   }
 
-  const toggleTheme = () =>
-  theme === themes.home
-  ? setTheme(themes.away)
-   : setTheme(themes.home);
+
   // const { isLoading } = useAuth0();
   // if (isLoading) return <div>Loading...</div>
 
   let page = null;
   if (view.name === "demo") {
     page =
-    <ThemeContext.Provider value={theme}>
+    // <ThemeContext.Provider value={theme}>
       <Demo changeView={changeView} />
-    </ThemeContext.Provider>
+    {/* </ThemeContext.Provider> */}
 
   } else {
     page = <Home changeView={changeView} />
   }
-  console.log(themes.dark)
+
   return (
     <>
       {page}
       {/* <Profile />
       <LogoutButton /> */}
-      <ThemeContext.Provider value={theme}>
-        <button style={themes} className="theme-btn" onClick={toggleTheme}>Change Theme</button>
 
-      </ThemeContext.Provider>
 
     </>
   )
